6837094dedb820f5a463fc0a127b70c51b9a4ef0
João Carlos Morais <85891987+jcmorais-araujo@users.noreply.github.com>
2021-09-14 08:15:40 -0300

    Add files via upload

---
+++ arrowFunctions.js
@@ -0,0 +1,21 @@
+// let multiplicador = function () {
+
+//     return 5 * 5;
+// }
+
+// console.log(multiplicador());
+
+// Arrow Functions
+
+// let multiplicador = () => {
+
+//     return 5 * 5;
+
+// };
+// console.log(multiplicador());
+
+let soma = (a, b) =>{
+    return a + b;
+};
+
+console.log(soma(5,2));
