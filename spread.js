37deda0088b1bfb238a0696e7e039fa3ff32c56e
João Carlos Morais <85891987+jcmorais-araujo@users.noreply.github.com>
2021-09-14 08:31:28 -0300

    Add files via upload

---
+++ spread.js
@@ -0,0 +1,7 @@
+let listaCursos1 = ["NodeJs e MongoDB", "Angular"];
+let listaCursos2 = ["Solid", "Laravel"];
+let listaCursos3 = ["Web completo", "Banco de dados"];
+
+let listaCompletaCursos = [...listaCursos1, ...listaCursos2, ...listaCursos3];
+
+console.log(listaCompletaCursos);
